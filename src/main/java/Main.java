a07c046c324e2c164934592cf5a3165e60800ce6
d1rannn <denchik.stars06@gmail.com>
2025-02-15 08:27:14 +0500

    Add Bean class & Test Bean in main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context =
+                new AnnotationConfigApplicationContext(
+                        ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+
+        System.out.println(person.getName());
+    }
+}
--- src/main/java/ProjectConfig.java
+++ src/main/java/ProjectConfig.java
@@ -3,4 +3,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
 
+    Person person() {
+        var p = new Person();
+        p.setName("John Doe");
+        return p;
+    }
 }
